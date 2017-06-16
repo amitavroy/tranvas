7e2711a8abd69e0df7488ed2f2de9b551582bdc0
Amitav Roy <reachme@amitavroy.com>
2017-06-16 17:02:23 +0530

    Just added events model, migration and controller

---
+++ app/Http/Controllers/Event/EventController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Event;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EventController extends Controller
+{
+    //
+}
---
+++ app/Modules/Event/Event.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Event;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $guarded = [];
+}
