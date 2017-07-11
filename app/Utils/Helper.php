29a6a5a550f20a3f67501bfe58502484a6ca84ff
Amitav Roy <reachme@amitavroy.com>
2017-07-11 07:33:49 +0530

    Fixed PHPCS error which failed Travas CI build pass

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -1,7 +1,6 @@
 <?php
 
-if (!function_exists('limit_words'))
-{
+if (!function_exists('limit_words')) {
     function limit_words($string, $words = 100, $end = '...')
     {
         return \Illuminate\Support\Str::words($string, $words, $end);
